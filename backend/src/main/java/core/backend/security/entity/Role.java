bd79cf63361961f5c9515ac53525b6ae24c89faa
wjdekdns1028 <wjdekdns0218@naver.com>
2025-02-07 13:18:44 +0900

    Fix:build.gradle and application.properties

---
+++ backend/src/main/java/core/backend/security/entity/Role.java
@@ -0,0 +1,6 @@
+package core.backend.security.entity;
+
+public enum Role {
+    ROLE_USER, //  일반 사용자
+    ROLE_ADMIN //  관리자
+}
---
+++ backend/src/main/java/core/backend/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package core.backend.security.repository;
+
+import core.backend.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+}
